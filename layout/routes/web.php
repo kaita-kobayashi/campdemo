eb8d5b427d1da34498adbf60884a43c7c307d362
KAITA_KOBAYASHI <kaita_kobayashi@PC-520.local>
2022-03-16 18:37:28 +0900

    ログインローティングを変更

--- layout/routes/web.php
+++ layout/routes/web.php
@@ -19,28 +19,40 @@ use function PHPUnit\Framework\returnSelf;
 */
 
 // ログイン・汎用系
-Route::get('/', function () {
-    if (Auth::check()) {
-        return redirect('home');
-    } else {
-        return redirect('login');
-    }
+Route::group(['middleware' => ['guest:web']], function () {
+    Route::get('/', function () {
+        if (Auth::check()) {
+            return redirect('home');
+        } else {
+            return redirect('login');
+        }
+    });
+    Route::get('login', [LoginController::class, 'create'])->name('login');
+    $limiter = config('fortify.limiters.login');
+    $twoFactorLimiter = config('fortify.limiters.two-factor');
+    $verificationLimiter = config('fortify.limiters.verification', '6,1');
+    Route::post('/login', [LoginController::class, 'store'])
+        ->middleware(array_filter([
+            'guest:' . config('fortify.guard'),
+            $limiter ? 'throttle:' . $limiter : null,
+        ]));
+    Route::post('/logout', [LoginController::class, 'destroy'])
+        ->name('logout');
+
+    Route::get('two_factor_auth', [LoginController::class, 'getTwoFactorAuth'])->name('getTwoFactorAuth');
+    Route::post('two_factor_auth', [LoginController::class, 'postTwoFactorAuth'])->name('postTwoFactorAuth');
+    Route::get('/exception', function () {
+        return view('errors.exception');
+    })->name('exception');
+    Route::get('first_login', [LoginController::class, 'getFirstLogin'])->name('getFirstLogin');
+    Route::post('first_login', [LoginController::class, 'postFirstLogin'])->name('postFirstLogin');
 });
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/home', function () {
-    return view('home');
-})->name('home');
-
-Route::get('two_factor_auth', [LoginController::class, 'getTwoFactorAuth'])->name('getTwoFactorAuth');
-Route::post('two_factor_auth', [LoginController::class, 'postTwoFactorAuth'])->name('postTwoFactorAuth');
-Route::get('/exception', function () {
-    return view('errors.exception');
-})->name('exception');
-Route::get('first_login', [LoginController::class, 'getFirstLogin'])->name('getFirstLogin');
-Route::post('first_login', [LoginController::class, 'postFirstLogin'])->name('postFirstLogin');
 
 // 画面
 Route::group(['middleware' => ['auth:web']], function () {
+    Route::get('home', function () {
+        return view('home');
+    })->name('home');
     // スタッフ管理
     Route::group(['prefix' => 'staff'], function () {
         Route::get('/', [StaffController::class, 'getStaff'])->name('getStaff');
